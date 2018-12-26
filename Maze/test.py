aee52a8da6111661ba9d6641dd284d7092a82830
Hsiao Chunhuo <jksparrow@users.noreply.github.com>
2018-12-26 09:43:57 +0800

    add code
    test random in list and string

---
+++ Maze/test.py
@@ -0,0 +1,19 @@
+import random
+import math
+import time
+
+'''
+for i in range (5):
+    string =  []
+    for j in range (10):
+        string.append(random.choice(['1','2','3','4']))
+    print('output : ')
+    print(string)
+
+'''
+for i in range (5):
+    string =  ''
+    for j in range (10):
+        string += random.choice(['1','2','3','4'])
+    print('output : ')
+    print(string)
